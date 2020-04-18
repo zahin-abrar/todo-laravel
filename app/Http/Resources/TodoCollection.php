63c5f40b6c9db295c23bd7c046d20284afed9e6a
Zahin <zahin.aiub@gmail.com>
2020-04-18 15:00:43 +0600

    Full Project

---
+++ app/Http/Resources/TodoCollection.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class TodoCollection extends JsonResource
+{
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this -> id,
+            'title' => $this -> title,
+            'completed' => $this -> completed
+        ];
+    }
+}
